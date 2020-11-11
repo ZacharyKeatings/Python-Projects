b5be95fa8068343874e885f11f175a10d559b740
ZacharyKeatings <74317231+ZacharyKeatings@users.noreply.github.com>
2020-11-11 16:25:15 -0500

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+start = int (input("Enter starting number: "))
+end = int (input("Enter ending number: "))
+
+for i in range (start, end + 1):
+	if (i%3==0 and i%5==0):
+		print("FizzBuzz")
+	elif (i%3==0):
+		print("Fizz")
+	elif (i%5==0):
+		print("Buzz")
+	else:
+		print(i)
